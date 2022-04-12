8cfb999530cc3660e71c7f15eef80dbdff781160
Raymond Loman <raymond@carpago.nl>
2022-04-12 09:25:23 +0200

    Add planning

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module concerts {
+	exports nl.youngcapital.concerts.service;
+	exports nl.youngcapital.concerts.model;
+	exports nl.youngcapital.concerts.constructors;
+	exports nl.youngcapital.concerts.statics;
+	exports nl.youngcapital.concerts.utils;
+	exports nl.youngcapital.concerts.interfaces;
+	exports nl.youngcapital.concerts.basics;
+}
