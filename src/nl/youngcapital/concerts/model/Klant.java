f259d1a7b1104e39469141a04c7c27e7aa3ccfe6
Raymond Loman <raymond@carpago.nl>
2022-04-12 12:17:04 +0200

    Add demo Klant.java for DB introduction. Add package...doc with some DB instructions

---
+++ src/nl/youngcapital/concerts/model/Klant.java
@@ -0,0 +1,43 @@
+package nl.youngcapital.concerts.model;
+
+public class Klant {
+
+	private long id;
+
+	private String voornaam;
+	private String achternaam;
+	private String adres;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getVoornaam() {
+		return voornaam;
+	}
+
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+
+	public String getAchternaam() {
+		return achternaam;
+	}
+
+	public void setAchternaam(String achternaam) {
+		this.achternaam = achternaam;
+	}
+
+	public String getAdres() {
+		return adres;
+	}
+
+	public void setAdres(String adres) {
+		this.adres = adres;
+	}
+
+}
